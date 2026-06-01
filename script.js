818dd54b9869c571aa6bb37fc93a0328752e9c68
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-06-01 16:19:10 -0400

    trying js instead of css for typewriting animation to deal with text length differences in each page header

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+
 document.addEventListener("click", () => {
-    const sound = new Audio("click.mp3");
-    sound.play();
+    const click = new Audio("click.mp3");
+    click.play();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector(".type");
+    const message = title.textContent;
+
+    title.textContent = "";
+
+    let i = 0;
+
+    function typeText() {
+        if (i < message.length) {
+            title.textContent += message[i];
+            i++;
+
+            setTimeout(typeText, 200);
+        } else {
+            title.classList.add("done");
+        }
+    }
+
+    typeText();
 });
